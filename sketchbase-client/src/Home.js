6c227ce325e9ead66c1a2c30d6896b4aadadc7a0
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2025-03-01 18:12:39 -0500

    Routing
    init Sidebar, Header components
    
    init header, sidebar components
    
    Profile Component
    
    Set up Routing: Home, Profiles
    
    install router

---
+++ sketchbase-client/src/Home.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5001/api/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>SketchBase Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/profile/${user.id}`}>
+              {user.firstName} {user.lastName}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
---
+++ sketchbase-client/src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+    return (
+        <div style={styles.sidebar}>
+        </div>
+    );
+};
+
+const styles = {
+    sidebar: {
+        background_color: "green",
+    }
+};
+
+export default Sidebar;
