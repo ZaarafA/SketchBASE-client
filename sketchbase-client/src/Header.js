a8006a7498f206c4c24e2df632418b3f5d9bbf1b
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2025-03-01 21:12:24 -0500

    init header, sidebar components

---
+++ sketchbase-client/src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div style={styles.header}>
+    </div>
+  );
+};
+
+const styles = {
+    header: {
+        background_color: "red",
+    }
+};
+
+export default Header;
