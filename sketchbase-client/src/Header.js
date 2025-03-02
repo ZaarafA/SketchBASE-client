97a8f3b971a363c780721d428c13be32b4559c23
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2025-03-01 21:13:24 -0500

    init Sidebar, Header components

--- sketchbase-client/src/Header.js
+++ sketchbase-client/src/Header.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div style={styles.header}>
-    </div>
-  );
+    return (
+        <div style={styles.header}>
+        </div>
+    );
 };
 
 const styles = {
--- sketchbase-client/src/Sidebar.js
+++ sketchbase-client/src/Sidebar.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return (
-    <div style={styles.sidebar}>
-    </div>
-  );
+    return (
+        <div style={styles.sidebar}>
+        </div>
+    );
 };
 
 const styles = {
