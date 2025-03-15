20bcb950611965a5e3bdec189af3e188f76009d6
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2025-03-14 23:33:31 -0400

    Separated Firebase config

--- sketchbase-client/src/App.js
+++ sketchbase-client/src/App.js
@@ -4,24 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Search from './Search';
-
-// FIREBASE CONFIG
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD1jNk4HgJsWF2zJRUA6M1IdYvMLLi1UYo",
-  authDomain: "sketchbase-fae70.firebaseapp.com",
-  projectId: "sketchbase-fae70",
-  storageBucket: "sketchbase-fae70.firebasestorage.app",
-  messagingSenderId: "6295064437",
-  appId: "1:6295064437:web:dd0c1132c65aebfc934d3c",
-  measurementId: "G-NEJQJHYDZX"
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
+import Login from './Login';
+import Signup from './Signup';
 
 function App() {
   return (
@@ -30,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profile/:userId" element={<Profile />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
