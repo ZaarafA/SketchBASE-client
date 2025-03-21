d45b7a885eecbbd71c99c84ddd9d0e2959a4584f
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2025-03-21 02:08:15 -0400

    Add Sidebar, Header to Signup Page

--- src/Signup.js
+++ src/Signup.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 import { auth } from "./firebase";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
@@ -22,22 +24,31 @@ const Signup = () => {
     };
 
     return (
-        <div className="signup-container">
-            <div className="signup-box">
-                <h1>Create Your Account</h1>
-                <p>Join our community and start showcasing your art today.</p>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Enter your name" value={username} onChange={(e) => setUsername(e.target.value)} required />
-                    <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                    <input type="password" placeholder="Create a password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                    <button type="submit" className="signup-button">Sign Up</button>
-                </form>
-                <button className="back-button" onClick={() => navigate("/")}>
-                    Back to Home
-                </button>
+        <div className="container">
+            <Header />
+            <div className="main">
+                <Sidebar />
+                <div className="content signup-container">
+                        <div className="signup-box">
+                            <h1>Create Your Account</h1>
+                            <p>Join our community and start showcasing your art today.</p>
+                            <form onSubmit={handleSubmit}>
+                                <input type="text" placeholder="Enter your name" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                                <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                                <input type="password" placeholder="Create a password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                                <button type="submit" className="signup-button">Sign Up</button>
+                            </form>
+                            <button className="back-button" onClick={() => navigate("/")}>
+                                Back to Home
+                            </button>
+                        </div>
+                </div>
             </div>
         </div>
+
     );
 };
+
+
 
 export default Signup;
