99425ac4bb54831ffedd28d0226514a9e162a5c6
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2025-05-04 01:28:03 -0400

    Remove Test buttons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -34,12 +34,9 @@ const Sidebar = () => {
                 <Link to="/search">Search</Link>
                 <Link to={user ? `/profile/${user.uid}` : "/login"}>Profile</Link>
                 {!user && (<Link to="/login">Login / Signup</Link>)}
-                {/* TEMP LINKS: FOR TESTING ONLY */}
-                {/* <Link to="/signup">*Sign Up</Link> */}
-                {/* <Link to="/test">*Faraaz Testing</Link> */}
                 </div>
             <div className="sidebar-section sidebar-middle">
-                <h3>Ongoing Orders</h3>
+                <h3></h3>
             </div>
             <div className="sidebar-section sidebar-bottom">
                 <div className="pfp-div">
